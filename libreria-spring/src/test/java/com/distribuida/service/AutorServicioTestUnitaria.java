39af1d1bedf9f14e8860f7654b014a88f6a4d9a7
DP <alejandrop@tecnologicoismac.edu.ec>
2025-06-06 12:32:39 -0500

    Actualizacion de datos

--- libreria-spring/src/test/java/com/distribuida/service/AutorServicioTestUnitaria.java
+++ libreria-spring/src/test/java/com/distribuida/service/AutorServicioTestUnitaria.java
@@ -10,9 +10,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,8 +47,69 @@ public class AutorServicioTestUnitaria {
         verify(autorRepository, times(1)).findAll();
     }
 
+    @Test
+    public void testFindOneExistente(){
+        when(autorRepository.findById(1)).thenReturn(Optional.of(autor));
+        Autor autor = autorService.findOne(1);
+        assertNotNull(autor);
+        assertEquals("Jose", autor.getNombre());
+    }
 
+    @Test
+    public void testFindOneNoExistente(){
+        when(autorRepository.findById(2)).thenReturn(Optional.empty());
+        Autor autor = autorService.findOne(2);
+        assertNull(autor);
+    }
 
+    @Test
+    public void testSave(){
+        when(autorRepository.save(autor)).thenReturn(autor);
+        Autor autor1 = autorService.save(autor);
+        assertNotNull(autor1);
+        assertEquals("Ecuador", autor1.getPais());
+    }
+
+    @Test
+    public void testUpdateExistente(){
+        Autor autorActualizado = new Autor();
+        autorActualizado.setNombre("Jose");
+        autorActualizado.setApellido("Gonzales");
+        autorActualizado.setPais("Mexico");
+        autorActualizado.setDireccion("Av. Club America");
+        autorActualizado.setTelefono("1532456132");
+        autorActualizado.setCorreo("jsoevsda22@gmail.com");
+
+        when(autorRepository.findById(1)).thenReturn(Optional.of(autor));
+        when(autorRepository.save(any())).thenReturn(autorActualizado);
+        Autor autorResultado = autorService.update(1, autorActualizado);
+        assertNotNull(autorResultado);
+        assertEquals("Jose", autorResultado.getNombre());
+        verify(autorRepository, times(1)).save(autor);
+    }
+
+    @Test
+    public void testUpdateNoExistente(){
+        Autor autorNuevo = new Autor();
+        when(autorRepository.findById(2)).thenReturn(Optional.empty());
+        Autor autorResultado = autorService.update(2, autorNuevo);
+        assertNull(autorResultado);
+        verify(autorRepository, never()).save(any());
+    }
+
+    @Test
+    public void testDeleteExistente(){
+        when(autorRepository.existsById(1)).thenReturn(true);
+        autorService.delete(1);
+        verify((autorRepository)).deleteById(1);
+    }
+
+    @Test
+    public void testDeleteNoExistente(){
+        when(autorRepository.existsById(2)).thenReturn(false);
+        autorService.delete(2);
+        verify(autorRepository, never()).deleteById(anyInt());
+    }
 
 
 
---
+++ libreria-spring/src/test/java/com/distribuida/service/CategoriaServicioTestUnitaria.java
@@ -0,0 +1,92 @@
+package com.distribuida.service;
+
+import com.distribuida.dao.CategoriaRepository;
+import com.distribuida.model.Categoria;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoriaServicioTestUnitaria {
+
+    @Mock
+    private CategoriaRepository categoriaRepository;
+
+    @InjectMocks
+    private CategoriaServiceImpl categoriaService;
+
+    private Categoria categoria;
+
+    @BeforeEach
+    public void setUp(){
+        categoria = new Categoria();
+        categoria.setIdCategoria(1);
+        categoria.setCategoria("Ciencia Ficcion");
+        categoria.setDescripcion("Libros de ciencia ficcion");
+    }
+
+    @Test
+    public void testFindAll(){
+        when(categoriaRepository.findAll()).thenReturn(List.of(categoria));
+        List<Categoria> categorias = categoriaService.findAll();
+        assertNotNull(categorias);
+        assertEquals(1, categorias.size());
+        verify(categoriaRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testFindOneExistente(){
+        when(categoriaRepository.findById(1)).thenReturn((Optional.of(categoria)));
+        Categoria categoria = categoriaService.findOne(1);
+        assertNotNull(categoria);
+        assertEquals("Ciencia Ficcion", categoria.getCategoria());
+    }
+
+    @Test
+    public void testFindOneNoExistente(){
+        when(categoriaRepository.findById(2)).thenReturn(Optional.empty());
+        Categoria categoria = categoriaService.findOne(2);
+        assertNull(categoria);
+    }
+
+    @Test
+    public void testSave(){
+        when(categoriaRepository.save(categoria)).thenReturn(categoria);
+        Categoria categoria1 = categoriaService.save(categoria);
+        assertNotNull(categoria1);
+        assertEquals("Ciencia Ficcion", categoria1.getCategoria());
+    }
+
+    @Test
+    public void testUpdateExistente(){
+        Categoria categoriaActualizada = new Categoria();
+        categoriaActualizada.setCategoria("Anime");
+        categoriaActualizada.setDescripcion("El mejor anime de todos los tiempos");
+
+        when(categoriaRepository.findById(1)).thenReturn(Optional.of(categoria));
+        when(categoriaRepository.save(any())).thenReturn(categoriaActualizada);
+        Categoria categoriaResultado = categoriaService.update(1, categoriaActualizada);
+        assertNotNull(categoriaResultado);
+        assertEquals("Anime", categoriaResultado.getCategoria());
+        verify(categoriaRepository, times(1)).save(categoria);
+    }
+
+    @Test
+    public void testUpdateNoExistente(){
+        Categoria categoriaNueva = new Categoria();
+        when(categoriaRepository.findById(2)).thenReturn(Optional.empty());
+        Categoria categoriaResultado = categoriaService.update(2,categoriaNueva);
+        assertNull(categoriaResultado);
+        verify(categoriaRepository, never()).save(any());
+    }
+
+}
