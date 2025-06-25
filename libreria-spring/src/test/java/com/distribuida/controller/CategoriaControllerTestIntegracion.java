ab14b055b99d2127b9b72b8894e86ae343159a9e
DP <alejandrop@tecnologicoismac.edu.ec>
2025-06-25 12:26:08 -0500

    Actualizacion reciente Controller

---
+++ libreria-spring/src/test/java/com/distribuida/controller/CategoriaControllerTestIntegracion.java
@@ -0,0 +1,62 @@
+package com.distribuida.controller;
+
+import com.distribuida.model.Categoria;
+import com.distribuida.service.CategoriaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.ManyToOne;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.MediaType;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.http.MediaType;
+
+@SuppressWarnings("removal")
+@WebMvcTest(CategoriaController.class)
+public class CategoriaControllerTestIntegracion {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CategoriaService categoriaService;
+
+    @Test
+    public void testFindAll() throws Exception {
+        Categoria categorias = new Categoria(1, "Accion", "Libros de acción");
+
+        Mockito.when(categoriaService.findAll()).thenReturn(List.of(categorias));
+
+        mockMvc.perform(get("/api/categorias"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].categoria").value("Accion"));
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        Categoria categorias = new Categoria(0,"Animada", "Libros animados");
+
+        Mockito.when(categoriaService.save(any(Categoria.class))).thenReturn(categorias);
+
+        mockMvc.perform((post("/api/categorias")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(categorias))
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.categoria").value("Animada")));
+    }
+}
